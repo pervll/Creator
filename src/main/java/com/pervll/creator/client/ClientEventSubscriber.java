75f1e168d2bee8888de92b51de4adfd8d8012077
pervll <wangziao1211@outlook.com>
2022-03-22 13:27:03 +0800

    try a speed modifier

--- src/main/java/com/pervll/creator/client/ClientEventSubscriber.java
+++ src/main/java/com/pervll/creator/client/ClientEventSubscriber.java
@@ -9,21 +9,27 @@ import com.pervll.creator.world.entity.monster.Alaif;
 import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityEvent;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.ItemAttributeModifierEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.ArrowLooseEvent;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.jline.utils.DiffHelper;
 
 @Mod.EventBusSubscriber(modid = Creator.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ClientEventSubscriber {
@@ -34,6 +40,13 @@ public class ClientEventSubscriber {
                 event.getEntityLiving().setHealth(1f);
                 event.setCanceled(true);
             }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onArrow(ItemAttributeModifierEvent event) {
+        if(event.getItemStack().getEquipmentSlot().equals(EquipmentSlot.HEAD)) {
+            event.addModifier(Attributes.MAX_HEALTH, new AttributeModifier("Speed Modify", 0.3d, AttributeModifier.Operation.ADDITION));
         }
     }
 }
